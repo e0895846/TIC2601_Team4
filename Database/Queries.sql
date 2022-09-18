de5b2f14097daa4c6479039d4e2cd0b2ee7bc5b1
e0895846 <113821555+e0895846@users.noreply.github.com>
2022-09-18 16:09:43 +0800

    Create posts table queries

--- Database/Queries.sql
+++ Database/Queries.sql
@@ -2,5 +2,15 @@ CREATE TABLE users (
   user_id INT PRIMARY KEY,
   name VARCHAR NOT NULL,
   password VARCHAR(60) NOT NULL, 
+  reputation INT DEFAULT 0,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE posts (
+  post_id INT PRIMARY KEY,
+  user_id INT NOT NULL REFERENCE users(user_id),
+  header VARCHAR(255) NOT NULL,
+  contents VARCHAR,
+  is_comment TINYINT NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 )
