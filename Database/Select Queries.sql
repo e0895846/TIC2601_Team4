439bd5e86263c2550a72dab29f0a6677ee7d008b
qian68 <113821839+qian68@users.noreply.github.com>
2022-09-24 12:42:55 +0800

    Update Select Queries.sql

--- Database/Select Queries.sql
+++ Database/Select Queries.sql
@@ -1,19 +1,20 @@
 -- Search function
 -- if title is selected as search criteria
-SELECT p.post_id, p.user_id, p.header, p.contents, p.update_at, p.created_at, count(p.post_id) as nosPost, count(p.post_id)- sum(p.is_comment) As nosTopics
-FROM posts p
-WHERE p.header like '%search word%'
-GROUP by p.user_id;
+SELECT p.post_id, p.user_id, u.name, p.header, p.contents, p.update_at, p.created_at
+FROM posts p, users u
+WHERE p.user_id =  u.user_id AND p.header LIKE '%search word%';
+
 
 -- if username is selected as search criteria
-SELECT p.post_id, p.user_id, p.header, p.contents, p.update_at, p.created_at, count(p.post_id) as nosPost, count(p.post_id)- sum(p.is_comment) As nosTopics
+SELECT p.post_id, p.user_id, u.name, p.header, p.contents, p.update_at, p.created_at
 FROM posts p
 LEFT OUTER JOIN users u ON p.user_id =  u.user_id
-WHERE u.name like '%search word%' 
-GROUP by p.user_id;
+WHERE u.name LIKE '%search word%';
 
 -- if description is selected as search criteria
-SELECT p.post_id, p.user_id, p.header, p.contents, p.update_at, p.created_at, count(p.post_id) as nosPost, count(p.post_id)- sum(p.is_comment) As nosTopics
-FROM posts p
-WHERE p.contents like '%search word%'
-GROUP by p.user_id;
+SELECT p.post_id, p.user_id, u.name, p.header, p.contents, p.update_at, p.created_at
+FROM posts p, users u
+WHERE p.user_id =  u.user_id AND p.contents LIKE '%search word%';
+
+
+-- No. of comments display
