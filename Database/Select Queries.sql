abacd32494b778813b4a4e6ad96bae62b58ece11
qian68 <113821839+qian68@users.noreply.github.com>
2022-09-24 11:25:59 +0800

    Update Select Queries.sql
    Added SQL for search function

--- Database/Select Queries.sql
+++ Database/Select Queries.sql
@@ -1,1 +1,19 @@
-SELECT * FROM posts
+-- Search function
+-- if title is selected as search criteria
+SELECT p.post_id, p.user_id, p.header, p.contents, p.update_at, p.created_at, count(p.post_id) as nosPost, count(p.post_id)- sum(p.is_comment) As nosTopics
+FROM posts p
+WHERE p.header like '%search word%'
+GROUP by p.user_id;
+
+-- if username is selected as search criteria
+SELECT p.post_id, p.user_id, p.header, p.contents, p.update_at, p.created_at, count(p.post_id) as nosPost, count(p.post_id)- sum(p.is_comment) As nosTopics
+FROM posts p
+LEFT OUTER JOIN users u ON p.user_id =  u.user_id
+WHERE u.name like '%search word%' 
+GROUP by p.user_id;
+
+-- if description is selected as search criteria
+SELECT p.post_id, p.user_id, p.header, p.contents, p.update_at, p.created_at, count(p.post_id) as nosPost, count(p.post_id)- sum(p.is_comment) As nosTopics
+FROM posts p
+WHERE p.contents like '%search word%'
+GROUP by p.user_id;
