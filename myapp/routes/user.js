4bd2bdb0bedcdfce4397a06bd84807d8df668625
Cai-XinCheng <e0895819@u.nus.edu>
2022-09-25 11:06:43 +0800

    clear files

--- myapp/routes/user.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/user', function(req, res, next) {
-  res.render('user', { title: 'Login Success Page' });
-});
-
-module.exports = router;
