abfc4bf7cdbc65e42689ce7e6a0d13d83dabe589
Cai-XinCheng <e0895819@u.nus.edu>
2022-10-08 17:21:06 +0800

    update header.ejs

--- Database/Create Queries.sql
+++ Database/Create Queries.sql
@@ -66,3 +66,29 @@ CREATE TABLE is_comment_of (
   child INT NOT NULL REFERENCES posts(post_id) ON DELETE CASCADE ON UPDATE CASCADE,
   PRIMARY KEY (parent, child)
 );
+
+TRUNCATE users;
+INSERT INTO users (username, password) VALUES 
+('Kelvin', '123456'),
+('James', '654321'),
+('Robert', '654321'),
+('John', '654321');
+
+TRUNCATE category;
+INSERT INTO category (category) VALUES
+('test');
+
+TRUNCATE data;
+INSERT INTO data (username, header, category, contents) VALUES 
+('Kelvin', 'TESTING1', 'test', 'This is testing 1 contents'),
+('James', 'TESTING2', 'test', 'This is testing 2 contents'),
+('Robert', 'TESTING3', 'test', 'This is testing 3 contents'),
+('John', 'TESTING4', 'test', 'This is testing 4 contents'),
+('James', 'TESTING5', 'test', 'This is testing 5 reply'),
+('Robert', 'TESTING3', 'test', 'This is testing 6 reply'),
+('John', 'TESTING4', 'test', 'This is testing 7 reply');
+
+INSERT INTO is_comment_of (parent, child) VALUES
+(1, 5),
+(1, 6),
+(5, 7);
