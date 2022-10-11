74476e55ee021169657bd5f273a1b099dc02fa24
e0895846 <e0895846@u.nus.edu>
2022-10-11 22:28:06 +0800

    Fix post upvote query

--- myapp/routes/post.js
+++ myapp/routes/post.js
@@ -8,9 +8,10 @@ router.get('/:id', async (req, res) => {
     let reply = req.body.content;
     
     try{
-        post = await queryAsync("SELECT d.*, v.is_upvote FROM data d LEFT JOIN vote v ON v.post_id = d.post_id WHERE d.post_id = ? OR (v.post_id = ? AND v.username = '?')", [id, id, req.session.user]);
+        post = await queryAsync("SELECT d.*, v.is_upvote FROM data d LEFT JOIN (SELECT * FROM vote v WHERE v.post_id = ? AND v.username = '?') v ON v.post_id = d.post_id WHERE d.post_id = ?;)", [id, req.session.user, id]);
         replies = await queryAsync('SELECT * FROM data WHERE post_id IN (SELECT child FROM is_comment_of WHERE parent = ?)', [id]);
 
+        console.log(post[0].is_upvote);
         res.render('post',{
             req:req,
             title: post[0].header,
