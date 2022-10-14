0dc31bda5bae517def0eee3c3d2e70d990166ea9
Cai-XinCheng <e0895819@u.nus.edu>
2022-10-14 19:07:34 +0800

    Protect SQL injection on Login

--- myapp/app.js
+++ myapp/app.js
@@ -1,7 +1,6 @@
 const path = require('path');
 const express = require('express');
 const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser');
 const session = require('express-session')
 
 const {
@@ -12,7 +11,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser())
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: false}))
 
 app.set('views', path.join(__dirname, 'views/pages/'));
 app.set('view engine', 'ejs');
@@ -24,6 +23,9 @@ app.use(session({
     
 }));
 
+app.use('/public', express.static(__dirname + "/public"))
+app.use('/partial', express.static(__dirname + "/views/partial"))
+
 var fs = require('fs')
 var routes = fs.readdirSync('./routes/')
 var routeDict = {}
@@ -34,9 +36,6 @@ for (var key in routeDict){
 	var webpath = key == 'index' ? '/' : '/' + key;
 	app.use(webpath, routeDict[key]);
 }
-
-app.use('/public', express.static(__dirname + "/public"))
-app.use('/partial', express.static(__dirname + "/views/partial"))
 
 
 var server = app.listen(8080, function() {
