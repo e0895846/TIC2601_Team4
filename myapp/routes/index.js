53c40a79a52002ed764d9e443e78ac6c41db3f79
e0895846 <e0895846@u.nus.edu>
2022-10-29 01:45:38 +0800

    Hot & Trending Sections

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -9,9 +9,10 @@ router.get('/', async (req, res, next) => {
   try {
     posts = await queryAsync('SELECT p.header, d.* , v.is_upvote FROM post p LEFT JOIN data d ON d.post_id = p.post_id LEFT JOIN (SELECT * FROM vote v WHERE v.username = ?) v ON d.post_id = v.post_id ORDER BY created_at DESC LIMIT 20;', [req.session.user]);
     categories = await queryAsync(db.getAllCategory);
+    topCategories = await queryAsync(db.getTopCategories);
     subscribes = await queryAsync(db.getAllSubscribes, [req.session.user]);
     subscribes['current'] = '/';
-    topCategories = await queryAsync('SELECT category FROM category ORDER BY reputation DESC LIMIT 5');
+    
 
     res.render('index', {
       req:req,
@@ -32,15 +33,61 @@ router.get('/search', async (req, res) => {
   try {
     posts = await queryAsync('SELECT p.header, d.*, v.is_upvote FROM post p LEFT JOIN data d ON p.post_id = d.post_id LEFT JOIN (SELECT * FROM vote v WHERE v.username = ?) v ON d.post_id = v.post_id WHERE p.header LIKE ? OR d.content LIKE ?', [req.session.user, `%${req.query.search_content}%`, `%${req.query.search_content}%`]);
     categories = await queryAsync(db.getAllCategory);
+    topCategories = await queryAsync(db.getTopCategories);
     subscribes = await queryAsync(db.getAllSubscribes, [req.session.user]);
     subscribes['current'] = '/';
-    trendingPost = await queryAsync('SELECT p.post_id, p.header FROM post p WHERE p.post_id IN (SELECT * FROM trending_post_id)');
 
     res.render('index', {
       req:req,
       title:"Search Results",
       categories: categories,
-      trendingPost:trendingPost,
+      topCategories:topCategories,
+      subscribes: subscribes,
+      posts: posts
+    });
+  } catch (error) {
+    console.log('SQL error', error);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+router.get('/trending', async (req, res) => {
+  let opt = req.query.selectPicker;
+  try {
+    posts = await queryAsync('SELECT p.header, d.* , v.is_upvote FROM post p LEFT JOIN data d ON d.post_id = p.post_id LEFT JOIN (SELECT * FROM vote v WHERE v.username = ?) v ON d.post_id = v.post_id ORDER BY (d.reputation / POW(TIMESTAMPDIFF(HOUR, d.created_at, NOW()) + 2, 1.8)) DESC LIMIT 20;', [req.session.user]);
+    categories = await queryAsync(db.getAllCategory);
+    topCategories = await queryAsync(db.getTopCategories);
+    subscribes = await queryAsync(db.getAllSubscribes, [req.session.user]);
+    subscribes['current'] = '/';
+
+    res.render('index', {
+      req:req,
+      title:"Trending",
+      categories: categories,
+      topCategories:topCategories,
+      subscribes: subscribes,
+      posts: posts
+    });
+  } catch (error) {
+    console.log('SQL error', error);
+    res.status(500).send('Something went wrong');
+  }
+});
+
+router.get('/hot', async (req, res) => {
+  let opt = req.query.selectPicker;
+  try {
+    posts = await queryAsync('SELECT p.header, d.* , v.is_upvote FROM post p LEFT JOIN data d ON d.post_id = p.post_id LEFT JOIN (SELECT * FROM vote v WHERE v.username = ?) v ON d.post_id = v.post_id ORDER BY (p.comments / POW(TIMESTAMPDIFF(HOUR, d.created_at, NOW()) + 2, 1.8)) DESC LIMIT 20;', [req.session.user]);
+    categories = await queryAsync(db.getAllCategory);
+    topCategories = await queryAsync(db.getTopCategories);
+    subscribes = await queryAsync(db.getAllSubscribes, [req.session.user]);
+    subscribes['current'] = '/';
+
+    res.render('index', {
+      req:req,
+      title:"Hot",
+      categories: categories,
+      topCategories:topCategories,
       subscribes: subscribes,
       posts: posts
     });
