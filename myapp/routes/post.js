3ec571466dee4e68d6492a60b64b1448e35f1810
e0895846 <e0895846@u.nus.edu>
2022-10-29 01:45:19 +0800

    Top Categories

--- myapp/routes/post.js
+++ myapp/routes/post.js
@@ -11,18 +11,18 @@ router.get('/:id', async (req, res) => {
     try{
         post = await queryAsync("SELECT p.header, d.*, v.is_upvote, ico.parent FROM data d LEFT JOIN post p ON p.post_id = d.post_id LEFT JOIN (SELECT * FROM vote v WHERE v.username = ?) v ON v.post_id = d.post_id LEFT JOIN is_comment_of ico ON ico.child = d.post_id WHERE d.post_id = ?", [req.session.user, id]);
         replies = await queryAsync('SELECT d.*, v.is_upvote FROM data d LEFT JOIN (SELECT * FROM vote v WHERE v.username = ?) v ON d.post_id = v.post_id WHERE d.post_id IN (SELECT child FROM is_comment_of WHERE parent = ?)', [req.session.user, id]);
-        isPost = await queryAsync('SELECT p.post_id FROM post p WHERE p.post_id = ?', [id]);
+        topCategories = await queryAsync(db.getTopCategories);
         subscribes = await queryAsync(db.getAllSubscribes, [req.session.user]);
         subscribes['current'] = post[0].category;
-        topCategories = await queryAsync('SELECT category FROM category ORDER BY reputation DESC LIMIT 5');
+        
+
         res.render('post',{
             req:req,
             title: post[0].header,
             subscribes: subscribes,
             post: post[0],
             replies: replies,
-            topCategories:topCategories,
-            isPost:isPost
+            topCategories:topCategories
         });
         
     }catch(error){
